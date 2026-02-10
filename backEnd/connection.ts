14c85871879afea9574dbdd20bf56e07730efab3
theo <theo.saint-amand@orange.fr>
2026-02-10 10:05:14 +0100

    fix(depend): add dotenv dependencies

--- backEnd/connection.ts
+++ backEnd/connection.ts
@@ -1,9 +1,11 @@
 import postgres from "postgres"
+import dotenv from "dotenv"
+dotenv.config()
 
-// const databaseUrl = process.env.DATABASE_URL
+const databaseUrl = process.env.DATABASE_URL
 
-// if (!databaseUrl) throw Error("No variable in .env")
+if (!databaseUrl) throw Error("No variable in .env")
 
-const sql = postgres("postgresql://postgres:zjhbwwrT6jcV759@localhost:5432/KART");
+const sql = postgres(databaseUrl);
 
 export default sql
