b0398efe1570e5a9d4e997133b2e989406f3001e
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2026-02-11 09:00:41 +0100

    fix(merge): conflicts were not resolved

--- frontEnd/src/pages/post/list.tsx
+++ frontEnd/src/pages/post/list.tsx
@@ -21,43 +21,6 @@ export default function PostList() {
 				<div key={post.id}>{post.content}</div>
 			))}
 
-<<<<<<< HEAD
-    async function fetchPosts() {
-      const response: Post[] = await fetch(
-        `http://localhost:3000/post/?offset=${offset}&limit=${limit}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        },
-      ).then((res) => res.json());
-      
-      console.log("Fetched posts:", response);
-
-      setIsLoading(false);
-      setPosts((prev) => [...prev, ...response]);
-    }
-
-    fetchPosts();
-  }, [offset]);
-
-  if (isLoading) return "Loading...";
-
-  console.log(posts);
-
-  return (
-    <div className="flex flex-col gap-2">
-      {posts.map((post) => (
-        <div key={post.id}>{post.content}</div>
-      ))}
-
-      <button type="button" onClick={() => setOffset((prev) => prev + limit)}>
-        Load more...
-      </button>
-    </div>
-  );
-=======
 			<button
 				type="button"
 				onClick={() => setLimit(prev => prev + initialLimit)}
@@ -66,5 +29,4 @@ export default function PostList() {
 			</button>
 		</div>
 	);
->>>>>>> 9c59f581707fdea0c6e51038805e9c3f2b45ec35
 }
