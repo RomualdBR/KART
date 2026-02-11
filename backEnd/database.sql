39c6758b90b8c6cf33da1231004e76414a792f92
RomualdBR <rbrisacier@gmail.com>
2026-02-11 16:19:28 +0100

    change bdd delete on cascade

--- backEnd/database.sql
+++ backEnd/database.sql
@@ -36,7 +36,9 @@ CREATE TABLE "follow" (
   "follower_id" integer NOT NULL
 );
 
-ALTER TABLE "post_like" ADD FOREIGN KEY ("post_id") REFERENCES "post" ("id");
+ALTER TABLE "post_like" ADD CONSTRAINT "post_like_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "post"("id") ON DELETE CASCADE;
+
+ALTER TABLE "post_comment" ADD CONSTRAINT "post_comment_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "post"("id") ON DELETE CASCADE;
 
 ALTER TABLE "post_like" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 
@@ -45,5 +47,3 @@ ALTER TABLE "post_comment" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 ALTER TABLE "follow" ADD FOREIGN KEY ("follower_id") REFERENCES "user" ("id");
 
 ALTER TABLE "follow" ADD FOREIGN KEY ("reciever_id") REFERENCES "user" ("id");
-
-ALTER TABLE "post_comment" ADD FOREIGN KEY ("post_id") REFERENCES "post" ("id");
